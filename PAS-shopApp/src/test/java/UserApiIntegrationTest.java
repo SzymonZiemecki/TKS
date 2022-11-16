489bbcdc8a9cee1b070d2d612fa3079141fe4fd2
Szymon Ziemecki <szymonj60@gmail.com>
2022-11-16 15:57:14 +0100

    test case for register fail

--- PAS-shopApp/src/test/java/UserApiIntegrationTest.java
+++ PAS-shopApp/src/test/java/UserApiIntegrationTest.java
@@ -1,33 +1,20 @@
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
-import com.pas.model.Address;
 import com.pas.model.dto.UserDTO;
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.MountableFile;
-
-import java.nio.file.Paths;
 
 import static com.jayway.restassured.RestAssured.given;
-import static org.testcontainers.shaded.org.apache.commons.lang.BooleanUtils.isTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
-public class integrationTest extends TestContainerInitializer {
+public class UserApiIntegrationTest extends TestContainerInitializer {
     @Test
-    public void checkEndpoint() throws InterruptedException, JsonProcessingException {
+    public void registerTwiceWithSameLoginThrowsErrorTest() throws InterruptedException, JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         RequestSpecification req = new RequestSpecBuilder().setBaseUri(baseUri).build();
@@ -44,12 +31,14 @@ public class integrationTest extends TestContainerInitializer {
         UserDTO user = objectMapper.readValue(json, UserDTO.class);
         Assertions.assertEquals(user.getFirstName(), "Szymon");
 
-
-/*        Response res = given(req)
-                .when()
-                .log().all()
+        String message = given(req)
                 .header("Content-Type", "application/json")
                 .body(json)
-                .post("/users/register");*/
+                .log().all()
+                .post("/users/register")
+                .then()
+                .statusCode(500)
+                .extract().body().asString();
+        assertTrue(message.contains("Login already taken"));
     }
 }
