be8f979e6d290524174192431337bb3c3a74e1ea
Wardzionn <101461957+Wardzionn@users.noreply.github.com>
2023-03-07 19:57:51 +0100

    added product service and product repository test
    * product service
    
    * product repository test

--- ApplicationCore/ApplicationServices/src/main/java/com/tks/services/ProductService.java
+++ ApplicationCore/ApplicationServices/src/main/java/com/tks/services/ProductService.java
@@ -1,15 +1,21 @@
 package com.tks.services;
 
+import com.tks.Product.Product;
 import com.tks.infrastructure.carts.AddToUserCartPort;
 import com.tks.infrastructure.products.AddProductPort;
 import com.tks.infrastructure.products.DeleteProductPort;
 import com.tks.infrastructure.products.GetProductPort;
 import com.tks.infrastructure.products.UpdateProductPort;
+import com.tks.userinterface.ProductUseCase;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
-public class ProductService {
+public class ProductService implements ProductUseCase {
 
     @Inject
     AddProductPort addProductPort;
@@ -19,4 +25,49 @@ public class ProductService {
     GetProductPort getProductPort;
     @Inject
     DeleteProductPort deleteProductPort;
+
+    @Override
+    public Product findById(UUID id) {
+        return getProductPort.findById(id);
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        return getProductPort.findAllProducts();
+    }
+
+    @Override
+    public List<Product> findByName(String name) {
+        return getProductPort.findByName(name);
+    }
+
+    @Override
+    public List<Product> findByProducer(String producer) {
+        return getProductPort.findByProducer(producer);
+    }
+
+    @Override
+    public Product addItem(Product product) {
+        return addProductPort.addItem(product);
+    }
+
+    @Override
+    public Product updateProduct(UUID id, Product product) {
+        return updateProductPort.updateProduct(id, product);
+    }
+
+    @Override
+    public void removeItem(UUID id) {
+        deleteProductPort.removeItem(id);
+    }
+
+    @Override
+    public boolean isInOngoingOrder(UUID productId) {
+        return getProductPort.isInOngoingOrder(productId);
+    }
+
+    @Override
+    public List<Product> getProducts(Optional<String> producer, Optional<String> name) {
+        return getProductPort.getProducts(producer, name);
+    }
 }
--- RepositoriesAdapters/src/test/java/ProductRepositoryTest.java
+++ RepositoriesAdapters/src/test/java/ProductRepositoryTest.java
@@ -1,2 +1,51 @@
+import data.model.OrderEnt;
+import data.product.ProductEnt;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class ProductRepositoryTest extends BaseTest{
+
+    @Test
+    void addProductTest() {
+        assertEquals(productEntRepository.findAll().size(), 0);
+        productEntRepository.add(prepareTv());
+        assertEquals(productEntRepository.findAll().size(), 1);
+        productEntRepository.add(prepareTv());
+        assertEquals(productEntRepository.findAll().size(), 2);
+        productEntRepository.add(prepareTv());
+        assertEquals(productEntRepository.findAll().size(), 3);
+    }
+
+    @Test
+    void deleteProductTest() {
+        ProductEnt productEnt = prepareTv();
+        assertEquals(productEntRepository.findAll().size(), 0);
+        productEntRepository.add(productEnt);
+        assertEquals(productEntRepository.findAll().size(), 1);
+        productEntRepository.delete(productEnt.getId());
+        assertEquals(productEntRepository.findAll().size(), 0);
+        productEntRepository.add(prepareTv());
+        assertEquals(productEntRepository.findAll().size(), 1);
+        productEntRepository.delete(UUID.randomUUID());
+        assertEquals(productEntRepository.findAll().size(), 1);
+    }
+
+    @Test
+    void updateProductTest() {
+        ProductEnt productEnt = prepareTv();
+        assertEquals(productEntRepository.findAll().size(), 0);
+        productEntRepository.add(productEnt);
+        assertEquals(productEntRepository.findAll().size(), 1);
+        assertEquals(productEntRepository.findById(productEnt.getId()).get().getPrice(), 20);
+        productEnt.setPrice(1000.0);
+        productEnt.setProducer("szymonnn");
+
+        productEntRepository.update(productEnt.getId(), productEnt);
+        assertEquals(productEntRepository.findById(productEnt.getId()).get().getPrice(), 1000.0);
+        assertEquals(productEntRepository.findById(productEnt.getId()).get().getProducer(), "szymonnn");
+    }
 }
